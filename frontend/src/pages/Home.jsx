e39c164d507c6fdba7d4fc3f9bc3f2b1757ca8d4
sterbye <simonak.damian123@gmail.com>
2024-02-15 16:41:11 +0100

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+const BASE_URL = "http://localhost:4000";
+
+function Home() {
+  const { workouts, dispatch } = useWorkoutsContext();
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(`${BASE_URL}/api/workouts`);
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+}
+
+export default Home;
